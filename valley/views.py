dbc82901a0834484a5bca3adcbe96cd0caa24c80
Di_Mok <dimokarz@yandex.ru>
2022-05-31 23:56:34 +0300

    Eurika

--- valley/views.py
+++ valley/views.py
@@ -8,31 +8,26 @@ def index(request):
     header = 'Выбор систем полива'
     title = ' - Выбор'
     valleyLst = Valley.objects.all().values()
-    statusLst = Status.objects.all().values()
+    statusLst = Status.objects.all()
     return render(request, 'index.html', {'header': header, 'title': title, 'valley': valleyLst, 'status': statusLst})
 
 
 def simple(request):
-    result = {'f1': 123, 'f2': 56 }
     first = int(request.GET.get('first')[5:])
-    if request.GET.get('second') != None:
+    if request.GET.get('second') is not None:
         second = request.GET.get('second')[5:]
-        valleyLst = Valley.objects.all().in_bulk([first, second]).values()
+        valleyLst = Status.objects.all().in_bulk([first, second]).values()
     else:
-        valleyLst = Valley.objects.all().in_bulk([first]).values()
-    valleyStat = Status.objects.all().in_bulk([first]).values()
+        valleyLst = Status.objects.all().in_bulk([first]).values()
     header = 'Управление'
     title = ' - Управление'
 
-    return render(request, 'simple.html', {'header': header, 'title': title, 'valleyLst': valleyLst, 'valleyStat': valleyStat, 'result': result})
+    return render(request, 'simple.html', {'header': header, 'title': title, 'valleyLst': valleyLst})
 
 
 def test(request):
     valleyLst = Valley.objects.all().values()
     statusLst = Status.objects.all().values()
     btnLst = ControlBtn().rusButtons
-
-
-
 
     return render(request, 'test.html', {'btnLst': btnLst, 'valleyLst': valleyLst, 'statusLst': statusLst})
