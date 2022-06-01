056a389d5c3b4bc7affcb5f6a27305f4c6c22018
Di_Mok <dimokarz@yandex.ru>
2022-06-01 23:43:18 +0300

    Before migration

--- valley/control.py
+++ valley/control.py
@@ -2,10 +2,10 @@ class ControlBtn:
 
     def __init__(self):
         self._btn = [
-            {'rele': '7-9', 'eng': 'Start', 'rus': 'Старт'},{'rele': '8-9', 'eng': 'Stop', 'rus': 'Стоп'},
-            {'rele': '7-10', 'eng': 'Forward', 'rus': 'Вперёд'},{'rele': '8-10', 'eng': 'Revers', 'rus': 'Назад'},
-            {'rele': '7-11', 'eng': 'Water On', 'rus': 'Вода Вкл'},{'rele': '8-11', 'eng': 'Water Off', 'rus': 'Вода Выкл'},
-            {'rele': '7-12', 'eng': 'Sis On', 'rus': 'Автостоп Вкл'},{'rele': '8-12', 'eng': 'Sis Off', 'rus': 'Автостоп Выкл'}
+            {'rele': '7-9', 'eng': 'Start', 'rus': 'Старт'}, {'rele': '8-9', 'eng': 'Stop', 'rus': 'Стоп'},
+            {'rele': '7-10', 'eng': 'Forward', 'rus': 'Вперёд'}, {'rele': '8-10', 'eng': 'Revers', 'rus': 'Назад'},
+            {'rele': '7-11', 'eng': 'Water On', 'rus': 'Вода Вкл'}, {'rele': '8-11', 'eng': 'Water Off', 'rus': 'Вода Выкл'},
+            {'rele': '7-12', 'eng': 'Sis On', 'rus': 'Автостоп Вкл'}, {'rele': '8-12', 'eng': 'Sis Off', 'rus': 'Автостоп Выкл'}
         ]
 
     def _selLng(self, selLng):
