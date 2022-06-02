8dac816774be44a9c0151cfcc5459e8ae0af9812
Di_Mok <dimokarz@yandex.ru>
2022-06-02 23:17:18 +0300

    Buttons

--- valley/control.py
+++ valley/control.py
@@ -2,10 +2,14 @@ class ControlBtn:
 
     def __init__(self):
         self._btn = [
-            {'rele': '7-9', 'eng': 'Start', 'rus': 'Старт', 'br': '0'}, {'rele': '8-9', 'eng': 'Stop', 'rus': 'Стоп', 'br': '1'},
-            {'rele': '7-10', 'eng': 'Forward', 'rus': 'Вперёд', 'br': '0'}, {'rele': '8-10', 'eng': 'Revers', 'rus': 'Назад', 'br': '1'},
-            {'rele': '7-11', 'eng': 'Water On', 'rus': 'Вода Вкл', 'br': '0'}, {'rele': '8-11', 'eng': 'Water Off', 'rus': 'Вода Выкл', 'br': '1'},
-            {'rele': '7-12', 'eng': 'Sis On', 'rus': 'Cтоп Вкл', 'br': '0'}, {'rele': '8-12', 'eng': 'Sis Off', 'rus': 'Cтоп Выкл', 'br': '1'}
+            {'rele': '7-9', 'eng': 'Start', 'rus': 'Старт', 'br': '0', 'class': 'btn_start btn-outline-success'},
+            {'rele': '8-9', 'eng': 'Stop', 'rus': 'Стоп', 'br': '1', 'class': 'btn_stop btn-outline-danger'},
+            {'rele': '7-10', 'eng': 'Forward', 'rus': 'Вперёд', 'br': '0', 'class': 'btn_start btn-outline-secondary'},
+            {'rele': '8-10', 'eng': 'Revers', 'rus': 'Назад', 'br': '1', 'class': 'btn_start btn-outline-secondary'},
+            {'rele': '7-11', 'eng': 'Water On', 'rus': 'Вода Вкл', 'br': '0', 'class': 'btn_start btn-outline-secondary'},
+            {'rele': '8-11', 'eng': 'Water Off', 'rus': 'Вода Выкл', 'br': '1', 'class': 'btn_start btn-outline-secondary'},
+            {'rele': '7-12', 'eng': 'Sis On', 'rus': 'Cтоп Вкл', 'br': '0', 'class': 'btn_start btn-outline-secondary'},
+            {'rele': '8-12', 'eng': 'Sis Off', 'rus': 'Cтоп Выкл', 'br': '1', 'class': 'btn_start btn-outline-secondary'}
         ]
 
     def _selLng(self, selLng):
