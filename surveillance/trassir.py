63b27010214af83fbad9b53930c00ff90f7a49aa
Di_Mok <dimokarz@yandex.ru>
2022-06-10 17:05:23 +0300

    Add Start Window

--- surveillance/trassir.py
+++ surveillance/trassir.py
@@ -7,30 +7,34 @@ import json
 # {'hls_player_version': 1, 'success': 1, 'token': 'typNCCc2'}
 # {'hls_player_version': 1, 'success': 1, 'token': 'is8W802Z'}
 
-# url = '192.168.1.100' #:1557/
-#
-#
-# def camera():
-#     cams = []
-#     cam_id = ['bw1VN7ee', 'K2078ygW', 'mXM7PSi0']
-#
-#     url_sid = 'https://192.168.1.100:18082/login?username=Test&password=12345'
-#     jdata = requests.get(url_sid, verify=False)
-#     sid = json.loads(jdata.text)
-#     sid = sid['sid']
-#
-#     for row in cam_id:
-#         url_token = 'https://192.168.1.100:18082/get_video?channel={}&container=mjpeg&quality=70&stream=sub&framerate=0&sid={}'.format(row, sid)
-#         jdata = requests.get(url_token, verify=False)
-#         token = json.loads(jdata.text)
-#         print(token)
-#         token = token['token']
-#         cams.append('http://192.168.1.100:1557/' + token)
-#
-#     return cams
-#
-#
-# print(camera())
+url = '192.168.1.100' #:1557/
 
-def videoUrl(cam1, cam2=0):
-    pass
+
+def camera():
+    cams = []
+    cam_id = ['bw1VN7ee', 'K2078ygW', 'mXM7PSi0']
+
+    url_sid = 'https://192.168.1.100:18082/login?username=Test&password=12345'
+    jdata = requests.get(url_sid, verify=False)
+    sid = json.loads(jdata.text)
+    sid = sid['sid']
+
+    for row in cam_id:
+        url_token = 'https://192.168.1.100:18082/get_video?channel={}&container=mjpeg&quality=70&stream=sub&framerate=0&sid={}'.format(row, sid)
+        jdata = requests.get(url_token, verify=False)
+        token = json.loads(jdata.text)
+        token = token['token']
+        cams.append('http://192.168.1.100:1557/' + token)
+
+    return cams
+
+def videoUrl(vall1, vall2=0):
+    if vall2 != 0:
+        valleyLst = Valley.objects.all().in_bulk([vall1, vall2]).values()
+    else:
+        valleyLst = Valley.objects.all().in_bulk([vall1]).values()
+
+    return valleyLst
+
+for row in videoUrl(1, 2):
+    print(row)
